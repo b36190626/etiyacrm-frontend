76d93e649dca575d76ff567bde49f7d446df9850
mustafa.tatlikatik <mustafa.tatlikatik@etiya.com>
2024-05-02 00:32:03 +0300

    update-css-all-project

--- src/app/features/auth/components/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { SelectButtonModule } from 'primeng/selectbutton';
 import { WarningPopupComponent } from '../../../../../shared/components/warning-popup/warning-popup.component';
 @Component({
   selector: 'etiya-login-form',
   standalone: true,
   imports: [
-    CommonModule, RouterModule, TranslateModule, WarningPopupComponent
+    CommonModule, RouterModule, TranslateModule, WarningPopupComponent, SelectButtonModule
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
@@ -16,8 +17,9 @@ import { WarningPopupComponent } from '../../../../../shared/components/warning-
 export class LoginFormComponent {
   showPassword: boolean = false;
   constructor(
-
   ){}
+
+
 
   togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
@@ -27,6 +29,9 @@ export class LoginFormComponent {
 
   togglePopup(event: Event) {
     event?.preventDefault();
-    this.showPopup = !this.showPopup;}
+    this.showPopup = !this.showPopup;
+  }
+
 }
+
 
--- src/shared/components/navbar/navbar.component.ts
+++ src/shared/components/navbar/navbar.component.ts
@@ -1,14 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    CommonModule,
+    CommonModule, RouterModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+
+  constructor(
+    private router: Router,
+  ){}
+
+  toggle = true;
+  status = 'Enable';
+  b2cClick(){
+    this.toggle = !this.toggle;
+    this.status = this.toggle ? 'Enable' : 'Disable';
+    this.router.navigate(['home/search']);
+  }
+ }
