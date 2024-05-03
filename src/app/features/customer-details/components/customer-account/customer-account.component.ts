113d854882a3d809712c5f805dc2a354bf9590f9
burcukocal <burcukocal97@gmail.com>
2024-05-03 18:44:50 +0300

    update searc result and add customer-account

---
+++ src/app/features/customer-details/components/customer-account/customer-account.component.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CustomerListItemDto } from '../../../customers/models/customer-list-item-dto';
+import { CustomerApiService } from '../../../customers/services/customerApi.service';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-customer-account',
+  standalone: true,
+  imports: [
+    CommonModule,
+    NgxPaginationModule,
+    RouterModule
+  ],
+  templateUrl: './customer-account.component.html',
+  styleUrl: './customer-account.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CustomerAccountComponent implements OnInit{
+  list: Array<CustomerListItemDto> = [];
+  p: number = 1;
+  selectedRow: number = -1;
+
+  constructor(
+    private customersApiService: CustomerApiService,
+    private change: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.getList();
+
+  }
+
+  getList() {
+      this.customersApiService.getList().subscribe(customers => {
+      this.list = customers;
+      this.change.markForCheck();
+    });
+  }
+
+
+
+  toggleAccordion(index: number) {
+    this.selectedRow = (this.selectedRow === index) ? -1 : index;
+    console.log("olmadı")
+  }
+}
--- src/app/features/customers/models/customer-list-item-dto.ts
+++ src/app/features/customers/models/customer-list-item-dto.ts
@@ -7,4 +7,33 @@ export interface CustomerListItemDto {
   nationalityId: string;
   accountNumber: string;
   gsmNumber: string;
+  billingAccounts: BillingAccounts;
+  contactMedium: ContactMedium;
+}
+
+interface ContactMedium{
+  mobilePhone: string;
+}
+interface BillingAccounts {
+  id: number;
+  status: string;
+  accountNumber: string;
+  accountName: string;
+  accountType: string;   // bu ne
+  orders: Orders;
+}
+
+interface Orders {
+  id: number;
+  offers: Offers;
+}
+
+interface Offers{
+  id: number;
+  type: string;
+  products: Products;
+}
+interface Products{
+  id: number;
+  name: string;
 }
