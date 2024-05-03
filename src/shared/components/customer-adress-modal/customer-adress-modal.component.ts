12a46a419bdb67cc2f7e6e5ebe563291d582b36b
burcukocal <burcukocal97@gmail.com>
2024-05-03 13:38:12 +0300

    update address css

--- src/shared/components/customer-adress-modal/customer-adress-modal.component.ts
+++ src/shared/components/customer-adress-modal/customer-adress-modal.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-adress-modal',
@@ -11,8 +13,30 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './customer-adress-modal.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CustomerAdressModalComponent {
-isFormValid: any;
+export class CustomerAdressModalComponent implements OnInit {
+  addressForm !: FormGroup;
+  isFormValid: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.addressForm = this.fb.group({
+      city: ['', Validators.required], //city{id, name olarak tutuluyor}
+      street: ['', Validators.required],
+      district: ['', Validators.required],
+      flatNumber: ['', Validators.required],
+      description: ['', Validators.required]
+    })
+
+    this.addressForm.statusChanges.subscribe(
+      status => {
+        this.isFormValid = status === 'VALID';
+        console.log(status);
+    });
+  }
 closeModal() {
 throw new Error('Method not implemented.');
 }
