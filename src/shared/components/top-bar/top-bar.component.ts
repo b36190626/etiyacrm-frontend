76a10d025fdfdaa791bd90852df13a42ad3ebe61
burcukocal <burcukocal97@gmail.com>
2024-05-03 15:07:28 +0300

    add top-bar

--- src/shared/components/top-bar/top-bar.component.ts
+++ src/shared/components/top-bar/top-bar.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
   imports: [
     CommonModule,
+    RouterLink
   ],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss',
