585e889d56baafeea6704a8b7817290b7025c425
mustafa.tatlikatik <mustafa.tatlikatik@etiya.com>
2024-05-04 20:00:35 +0300

    navbar-fix

--- src/app/shared/components/top-bar/top-bar.component.ts
+++ src/app/shared/components/top-bar/top-bar.component.ts
@@ -1,6 +1,6 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { RouterLink, RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-top-bar',
