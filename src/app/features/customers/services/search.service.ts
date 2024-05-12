7152b97dd42d5abfd412836877529cca7bd19a12
burcukocal <burcukocal97@gmail.com>
2024-05-12 18:44:34 +0300

    add search filter api

--- src/app/features/customers/services/search.service.ts
+++ src/app/features/customers/services/search.service.ts
@@ -1,10 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CustomerDetailsDto } from '../models/customer/customer-details-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getByNationalityIdentity(nationalityId: string): Observable<CustomerDetailsDto> {
+    return this.http.get<CustomerDetailsDto>(`http://localhost:8082/api/v1/search-service/${nationalityId}`)
+  }
 
 }
