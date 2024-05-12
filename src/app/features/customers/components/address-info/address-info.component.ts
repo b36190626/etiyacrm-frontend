cf6f17d7955187bf56b66d75fe1f9ac73fb2955d
B.Kaan Yumak <yumakkaan@gmail.com>
2024-05-12 10:33:48 +0300

    update major-breakdownstates-address

--- src/app/features/customers/components/address-info/address-info.component.ts
+++ src/app/features/customers/components/address-info/address-info.component.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { CustomerAdressModalComponent } from '../../../../shared/components/customer-adress-modal/customer-adress-modal.component';
+import { CreateAddressRequest } from '../../models/address/requests/create-address-request';
+import { Observable } from 'rxjs';
+import { select, Store } from '@ngrx/store';
+import { selectAllAddresses } from '../../../../shared/stores/addresses/address.selector';
 
 
 @Component({
   selector: 'app-address-info',
+  template: `
+  <div *ngFor="let address of addresses$ | async">
+    <p>{{ address.street }}, {{ address.city }}</p>
+  </div>
+`,
   standalone: true,
   imports: [
     CommonModule,
@@ -16,14 +25,22 @@ import { CustomerAdressModalComponent } from '../../../../shared/components/cust
   styleUrl: './address-info.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddressInfoComponent {
+export class AddressInfoComponent implements OnInit {
+  addresses$: Observable<CreateAddressRequest[]>;
   addressList: Array<any>=["samipaşazade"]; //doldur
   optionClick: boolean=true;
   form: any;
   showPopup: boolean = false;
+
 constructor(
   private router: Router,
-){}
+  private store: Store
+){
+  this.addresses$ = this.store.pipe(select(selectAllAddresses));
+}
+  ngOnInit(): void {
+
+  }
 
 togglePopup(event: Event) {
   event?.preventDefault();
--- src/app/shared/stores/addresses/address.action.ts
+++ src/app/shared/stores/addresses/address.action.ts
@@ -2,6 +2,6 @@ import { createAction, props } from "@ngrx/store";
 import { CreateAddressRequest } from "../../../features/customers/models/address/requests/create-address-request";
 
 export const setAddress = createAction(
-  '[Address] set Address',
+  '[Addresses] Set Address',
   props<{ address: CreateAddressRequest }>()
 );
