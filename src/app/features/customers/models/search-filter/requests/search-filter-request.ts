211712c5da726b514f5b8ed58bc729a65505bcf3
mustafa.tatlikatik <mustafa.tatlikatik@etiya.com>
2024-05-13 18:11:06 +0300

    search-filter-models

---
+++ src/app/features/customers/models/search-filter/requests/search-filter-request.ts
@@ -0,0 +1,10 @@
+export interface SearchFilterRequest{
+    nationalityIdentity: string;
+    id: string;
+    accountNumber: string;
+    mobilePhone: string;
+    firstName: string;
+    lastName: string;
+    orderNumber: string;
+  }
+  
