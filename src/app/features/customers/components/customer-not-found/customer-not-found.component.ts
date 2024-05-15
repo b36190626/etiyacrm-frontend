b9155ec000e5874d7dadb4f32e27b61f497f8fc7
burcukocal <burcukocal97@gmail.com>
2024-05-15 21:32:02 +0300

    add error message

--- src/app/features/customers/components/customer-not-found/customer-not-found.component.ts
+++ src/app/features/customers/components/customer-not-found/customer-not-found.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { WarningPopupComponent } from '../../../../shared/components/warning-popup/warning-popup.component';
 
 @Component({
   selector: 'app-customer-not-found',
@@ -8,10 +9,13 @@ import { RouterLink, RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     RouterModule,
-    RouterLink
+    RouterLink,
+    WarningPopupComponent
   ],
   templateUrl: './customer-not-found.component.html',
   styleUrl: './customer-not-found.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CustomerNotFoundComponent { }
+export class CustomerNotFoundComponent {
+  message: string = 'Matching credentials not found.';
+}
