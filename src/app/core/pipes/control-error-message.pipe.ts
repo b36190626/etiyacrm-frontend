f400a7d384e295e3616f59519fd92549416a4543
burcukocal <burcukocal97@gmail.com>
2024-05-18 22:59:10 +0300

    update

--- src/app/core/pipes/control-error-message.pipe.ts
+++ src/app/core/pipes/control-error-message.pipe.ts
@@ -17,10 +17,9 @@ export class ControlErrorMessagePipe implements PipeTransform {
             control['loginError']? 'Wrong username or password. Please try again..':
             control['loginEmpty']? 'Username and password fields cant be left empty.':
             control['loginCharacter']? 'Username field does not allow Turkish characters, numbers, or symbols.':
-            control['loginPassword']? 'Password can not be less than 8 characters.':
             control['credential']? 'Matching credentials not found.':
             control['pattern']? 'Invalid format.':
-            control['minlength'] ? `This field must be at least ${control['minlength'].requiredLength} characters but it has ${control['minlength'].actualLength} characters.` :
+            control['minlength'] ? `Password cannot be less than 8 characters` :
             control['maxlength'] ? `This field must be maximum ${control['maxlength'].requiredLength} characters but it has ${control['maxlength'].actualLength} characters.` :
             control['email']? 'This is not a valid e-mail format Correct usage: example@email.com.':
             control['nationalityIdNotValid']? 'This Nationality ID is not valid.':
